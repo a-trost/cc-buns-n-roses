f1a8b0fa582be5cd139c9063f7457e564d481ecd
Alex Trost <alexrtrost@gmail.com>
2021-06-02 12:31:06 -0400

    Initial Commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+const { withPlugins } = require("next-compose-plugins");
+
+const nextConfig = { target: "serverless" };
+
+const withTM = require("next-transpile-modules")(["next-slicezone"]);
+
+module.exports = withPlugins([withTM()], nextConfig);
