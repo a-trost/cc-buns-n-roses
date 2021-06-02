2cc88f1e905a79c7e36c8bc7cb3773d04eb9fb54
Alex Trost <alexrtrost@gmail.com>
2021-06-02 13:09:30 -0400

    Add Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
-  stories: ["../slices/**/*.stories.[tj]s", "../components/**/*.stories.[tj]s"],
+  stories: [
+    "../slices/**/*.stories.[tj]s",
+    "../.slicemachine/assets/slices/**/*.stories.[tj]s",
+    "../components/**/*.stories.[tj]s",
+  ],
   addons: ["@storybook/preset-scss"],
 };
